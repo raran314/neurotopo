11b0f033701a32775683ac8527a114b20064e903
raran314 <123431219+raran314@users.noreply.github.com>
2024-05-28 15:40:53 -0400

    Update neuron_data_linking_number.py
    supposed to calculate average linking number in each window, but very very slow

--- neuron_data_linking_number.py
+++ neuron_data_linking_number.py
@@ -25,11 +25,51 @@ def sort_curve(rr):
     sorted_indices = np.argsort(slist)
     return centroid + rr_centered[sorted_indices]
 
+def compute_normal_vector(v1, v2):
+    return np.cross(v1, v2) / np.linalg.norm(np.cross(v1, v2))
+
+def dihedral_angle(n1, n2):
+    return np.arcsin(np.dot(n1, n2))
+
+def gauss_linking_integral(polygon1, polygon2):
+    n = len(polygon1) - 1
+    m = len(polygon2) - 1
+    L = 0
+
+    for i in range(n):
+        for j in range(m):
+            p_i = np.array(polygon1[i])
+            p_ip1 = np.array(polygon1[i + 1])
+            r_j = np.array(polygon2[j])
+            r_jp1 = np.array(polygon2[j + 1])
+
+            r_ij = p_i - r_j
+            r_ij1 = p_i - r_jp1
+            r_i1j = p_ip1 - r_j
+            r_i1j1 = p_ip1 - r_jp1
+
+            n1 = compute_normal_vector(r_ij, r_ij1)
+            n2 = compute_normal_vector(r_ij1, r_i1j1)
+            n3 = compute_normal_vector(r_i1j1, r_i1j)
+            n4 = compute_normal_vector(r_i1j, r_ij)
+
+            area_Qij = (
+                dihedral_angle(n1, n2) +
+                dihedral_angle(n2, n3) +
+                dihedral_angle(n3, n4) +
+                dihedral_angle(n4, n1)
+            )
+
+            L += area_Qij / (4 * np.pi)
+
+    return L
+
 def visualize_curve(ax, curve):
     curve = sort_curve(curve)
     ax.plot(curve[:,0], curve[:,1], curve[:,2], '-', linewidth=0.7, markersize=2)
 
 def visualize_in_window(curves, L):
+    linking_numbers = []
     # Find the range of data
     min_x = min([np.min(curve[:, 0]) for curve in curves])
     max_x = max([np.max(curve[:, 0]) for curve in curves])
@@ -40,20 +80,28 @@ def visualize_in_window(curves, L):
 
     # Loop over windows
     for x in np.arange(min_x, max_x, L):
+        print('hi')
         for y in np.arange(min_y, max_y, L):
             for z in np.arange(min_z, max_z, L):
-                fig = plt.figure()
-                ax = fig.add_subplot(111, projection='3d')
-                ax.set_xlim(x, x + L)
-                ax.set_ylim(y, y + L)
-                ax.set_zlim(z, z + L)
-                for curve in curves:
-                    curve_in_window = curve[(curve[:, 0] >= x) & (curve[:, 0] < x + L) &
-                                             (curve[:, 1] >= y) & (curve[:, 1] < y + L) &
-                                             (curve[:, 2] >= z) & (curve[:, 2] < z + L)]
-                    if len(curve_in_window) > 0:
-                        visualize_curve(ax, curve_in_window)
-                plt.show()
+                linking_number_window = 0
+                num_pairs = 0
+                for i, curve1 in enumerate(curves):
+                    for j, curve2 in enumerate(curves):
+                        if i != j:
+                            curve1_in_window = curve1[(curve1[:, 0] >= x) & (curve1[:, 0] < x + L) &
+                                                       (curve1[:, 1] >= y) & (curve1[:, 1] < y + L) &
+                                                       (curve1[:, 2] >= z) & (curve1[:, 2] < z + L)]
+                            curve2_in_window = curve2[(curve2[:, 0] >= x) & (curve2[:, 0] < x + L) &
+                                                       (curve2[:, 1] >= y) & (curve2[:, 1] < y + L) &
+                                                       (curve2[:, 2] >= z) & (curve2[:, 2] < z + L)]
+                            if len(curve1_in_window) > 0 and len(curve2_in_window) > 0:
+                                linking_number_window += gauss_linking_integral(curve1_in_window, curve2_in_window)
+                                num_pairs += 1
+                if num_pairs > 0:
+                    linking_numbers.append(linking_number_window / num_pairs)
+
+    average_linking_number = np.mean(linking_numbers)
+    print(f"Average linking number over all connected components within each window: {average_linking_number}")
 
 swc_file_path = '/Users/romyaran/Desktop/Summer 2024/confocal_stack.swc'
 dta = pd.read_csv(swc_file_path, delim_whitespace=True, header=None, comment='#', names=['n', 'type', 'x', 'y', 'z', 'r', 'parent'])
@@ -61,12 +109,6 @@ dta = pd.read_csv(swc_file_path, delim_whitespace=True, header=None, comment='#'
 G = swc_to_graph(dta)
 
 conncomp = list(nx.connected_components(G))
-# Convert connected components sets to indices
-cc_indices = [np.array(list(cc)) - 1 for cc in conncomp]
-
-# Extract curves based on indices
+cc_indices = [list(map(lambda x: x - 1, cc)) for cc in conncomp]
 curves = [dta.iloc[cc][['x', 'y', 'z']].values for cc in cc_indices]
-
-visualize_in_window(curves, L=50)  # Adjust the window size L as needed
-
-
+visualize_in_window(curves, L=5)  # Adjust the window size L as needed
