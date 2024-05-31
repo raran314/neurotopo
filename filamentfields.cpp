72275163bcde7dc3d1061dc218e4bddfc03adba1
raran314 <123431219+raran314@users.noreply.github.com>
2024-05-31 13:39:29 -0400

    Update filamentfields.cpp

--- filamentfields.cpp
+++ filamentfields.cpp
@@ -2,11 +2,10 @@
 
 #include "filamentFields.h"
 #include <iostream>
-#include <armadillo>
 
 filamentFields::filamentFields(const std::vector<arma::mat>& filament_nodes_list) :
-    filament_nodes_list(filament_nodes_list)
-{
+    filament_nodes_list(filament_nodes_list) {
+
     get_all_nodes();
     get_all_edges();
     get_node_labels();
@@ -15,33 +14,33 @@ filamentFields::filamentFields(const std::vector<arma::mat>& filament_nodes_list
 
 void filamentFields::get_all_nodes() {
     for (const arma::mat& nodes : filament_nodes_list) {
-        arma::mat resizedNodes = arma::zeros(nodes.n_rows, 3);
-        resizedNodes.cols(0, 2) = nodes.cols(0, 2);
-        all_nodes = arma::join_cols(all_nodes, resizedNodes);
+        all_nodes.resize(all_nodes.n_rows + nodes.n_rows, 3);
+        all_nodes.tail_rows(nodes.n_rows) = nodes;
     }
 }
 
 void filamentFields::get_all_edges() {
     filament_edges_list.clear();
-    all_edges = arma::zeros(0, 6); // Ensure all_edges is an Nx6 matrix
+    all_edges.resize(0, 6);
 
     for (const arma::mat& nodes : filament_nodes_list) {
         arma::mat edges(nodes.n_rows - 1, 6);
 
-        for (int idx = 0; idx < nodes.n_rows - 1; ++idx) {
-            edges(idx, arma::span(0, 2)) = nodes.row(idx);      // Start point of the edge
-            edges(idx, arma::span(3, 5)) = nodes.row(idx + 1);  // End point of the edge
+        for (size_t idx = 0; idx < nodes.n_rows - 1; ++idx) {
+            edges.row(idx).cols(0, 2) = nodes.row(idx);
+            edges.row(idx).cols(3, 5) = nodes.row(idx + 1);
         }
         filament_edges_list.push_back(edges);
     }
 
     for (const arma::mat& edges : filament_edges_list) {
-        all_edges = arma::join_cols(all_edges, edges);
+        all_edges.resize(all_edges.n_rows + edges.n_rows, 6);
+        all_edges.tail_rows(edges.n_rows) = edges;
     }
 }
 
 void filamentFields::get_node_labels() {
-    node_labels = arma::zeros<arma::ivec>(all_nodes.n_rows);
+    node_labels = arma::vec(all_nodes.n_rows, arma::fill::zeros);
     int label = 0;
     int cursor = 0;
     for (const arma::mat& nodes : filament_nodes_list) {
@@ -52,7 +51,7 @@ void filamentFields::get_node_labels() {
 }
 
 void filamentFields::get_edge_labels() {
-    edge_labels = arma::zeros<arma::ivec>(all_edges.n_rows);
+    edge_labels = arma::vec(all_edges.n_rows, arma::fill::zeros);
     int label = 0;
     int cursor = 0;
     for (const arma::mat& edges : filament_edges_list) {
@@ -62,13 +61,13 @@ void filamentFields::get_edge_labels() {
     }
 }
 
-arma::ivec filamentFields::sample_edges_locally(const arma::vec& query_point, double R_omega) const {
-    arma::ivec local_edge_labels = arma::zeros<arma::ivec>(all_edges.n_rows);
-    for (int idx = 0; idx < all_edges.n_rows; ++idx) {
-        arma::vec edge_start = all_edges.row(idx).subvec(0, 2);
-        arma::vec edge_end = all_edges.row(idx).subvec(3, 5);
+arma::vec filamentFields::sample_edges_locally(const arma::vec& query_point, double R_omega) const {
+    arma::vec local_edge_labels = arma::vec(all_edges.n_rows, arma::fill::zeros);
+    for (size_t idx = 0; idx < all_edges.n_rows; ++idx) {
+        arma::vec edge_start = all_edges.row(idx).cols(0, 2).t();
+        arma::vec edge_end = all_edges.row(idx).cols(3, 5).t();
 
-        if (((edge_start - query_point).norm() < R_omega) && ((edge_end - query_point).norm() < R_omega)) {
+        if (arma::norm(edge_start - query_point) < R_omega && arma::norm(edge_end - query_point) < R_omega) {
             local_edge_labels(idx) = 1;
         }
     }
@@ -76,11 +75,9 @@ arma::ivec filamentFields::sample_edges_locally(const arma::vec& query_point, do
 }
 
 arma::mat filamentFields::analyzeLocalVolume(const arma::vec& query_point, double R_omega, double rod_radius) {
-    // Sample edges locally
-    arma::ivec local_edge_labels = sample_edges_locally(query_point, R_omega);
+    arma::vec local_edge_labels = sample_edges_locally(query_point, R_omega);
     number_of_labels = 0;
-    // Count the number of local edges
-    int local_edge_count = arma::accu(local_edge_labels);
+    int local_edge_count = arma::sum(local_edge_labels);
     if (local_edge_count == 0) {
         number_of_labels = 0;
         volume_fraction = std::numeric_limits<double>::quiet_NaN();
@@ -89,17 +86,16 @@ arma::mat filamentFields::analyzeLocalVolume(const arma::vec& query_point, doubl
         return arma::mat(0, 6);
     }
 
-    // Extract local edges
     arma::mat local_edges(local_edge_count, 6);
-    arma::ivec local_edge_indices = arma::zeros<arma::ivec>(local_edge_count);
+    arma::vec local_edge_indices = arma::vec(local_edge_count, arma::fill::zeros);
     int index = 0;
-    for (int i = 0; i < all_edges.n_rows; ++i) {
+    for (size_t i = 0; i < all_edges.n_rows; ++i) {
         if (local_edge_labels(i) == 1) {
             local_edge_indices(index) = i;
             local_edges.row(index++) = all_edges.row(i);
         }
     }
-    // Unique labels
+
     std::vector<int> unique_labels;
     for (int i = 0; i < local_edge_count; ++i) {
         if (std::find(unique_labels.begin(), unique_labels.end(), local_edge_labels(i)) == unique_labels.end()) {
@@ -110,34 +106,29 @@ arma::mat filamentFields::analyzeLocalVolume(const arma::vec& query_point, doubl
 
     double edge_length_sum = 0.0;
     for (int i = 0; i < local_edge_count; ++i) {
-        arma::vec edge_start = local_edges.row(i).subvec(0, 2);
-        arma::vec edge_end = local_edges.row(i).subvec(3, 5);
+        arma::vec edge_start = local_edges.row(i).cols(0, 2).t();
+        arma::vec edge_end = local_edges.row(i).cols(3, 5).t();
         edge_length_sum += arma::norm(edge_end - edge_start);
     }
 
-    // volume fraction
     volume_fraction = (M_PI * rod_radius * rod_radius * edge_length_sum) / (4.0 / 3.0 * M_PI * R_omega * R_omega * R_omega);
 
-    // Orientational order parameter
-    arma::mat Q = arma::zeros(3, 3);
+    arma::mat Q = arma::mat(3, 3, arma::fill::zeros);
     for (int i = 0; i < local_edge_count; ++i) {
-        arma::vec edge_start = local_edges.row(i).subvec(0, 2);
-        arma::vec edge_end = local_edges.row(i).subvec(3, 5);
+        arma::vec edge_start = local_edges.row(i).cols(0, 2).t();
+        arma::vec edge_end = local_edges.row(i).cols(3, 5).t();
         arma::vec edge = edge_end - edge_start;
         edge /= arma::norm(edge);
-        Q += (3.0 * edge * edge.t() - arma::eye<arma::mat>(3, 3)) / 2.0;
+        Q += (3.0 * edge * edge.t() - arma::mat(3, 3, arma::fill::eye)) / 2.0;
     }
     Q /= local_edge_count;
-    arma::vec eigval;
-    arma::mat eigvec;
-    arma::eig_sym(eigval, eigvec, Q);
-    double max_eigenvalue = arma::max(arma::abs(eigval));
+    arma::vec eigenvalues = arma::eig_sym(Q);
+    double max_eigenvalue = arma::abs(eigenvalues).max();
     orientational_order_parameter = max_eigenvalue;
 
-    // Entanglement
-    arma::mat entanglement_matrix(local_edge_count, local_edge_count, arma::fill::zeros);
-    compute_edge_wise_entanglement(local_edges, local_edge_labels, entanglement_matrix);
-    entanglement = arma::accu(arma::abs(entanglement_matrix));
+    arma::mat entanglement_matrix(local_edge_count, local_edge_count, arma::fill::nan);
+    filamentFields::compute_edge_wise_entanglement(local_edges, local_edge_labels, entanglement_matrix);
+    entanglement = arma::accu(entanglement_matrix.transform([](double val) { return std::isnan(val) ? 0.0 : std::abs(val); }));
 
     return local_edges;
 }
@@ -146,4 +137,47 @@ double filamentFields::_clip(double x, double lower, double upper) const {
     return std::min(std::max(x, lower), upper);
 }
 
-double filamentFields::compute_linking_number_for_edges(const
+double filamentFields::compute_linking_number_for_edges(const arma::rowvec& e_i, const arma::rowvec& e_j) const {
+    arma::vec r_ij = e_i.cols(0, 2).t() - e_j.cols(0, 2).t();
+    arma::vec r_ijj = e_i.cols(0, 2).t() - e_j.cols(3, 5).t();
+    arma::vec r_iij = e_i.cols(3, 5).t() - e_j.cols(0, 2).t();
+    arma::vec r_iijj = e_i.cols(3, 5).t() - e_j.cols(3, 5).t();
+
+    double tol = 1e-6;
+
+    arma::vec n1 = arma::cross(r_ij, r_ijj);
+    n1 /= (arma::norm(n1) + tol);
+
+    arma::vec n2 = arma::cross(r_ijj, r_iijj);
+    n2 /= (arma::norm(n2) + tol);
+
+    arma::vec n3 = arma::cross(r_iijj, r_iij);
+    n3 /= (arma::norm(n3) + tol);
+
+    arma::vec n4 = arma::cross(r_iij, r_ij);
+    n4 /= (arma::norm(n4) + tol);
+
+    return -1.0 / (4 * M_PI) * std::abs(
+        std::asin(filamentFields::_clip(arma::dot(n1, n2), -1.0 + tol, 1.0 - tol)) +
+        std::asin(filamentFields::_clip(arma::dot(n2, n3), -1.0 + tol, 1.0 - tol)) +
+        std::asin(filamentFields::_clip(arma::dot(n3, n4), -1.0 + tol, 1.0 - tol)) +
+        std::asin(filamentFields::_clip(arma::dot(n4, n1), -1.0 + tol, 1.0 - tol))
+    );
+}
+
+void filamentFields::compute_edge_wise_entanglement(const arma::mat& _all_edges, const arma::vec& labels, arma::mat& entanglement_matrix) const {
+    size_t num_all_edges = _all_edges.n_rows;
+    for (size_t idx = 0; idx < num_all_edges; ++idx) {
+        const arma::rowvec edge1 = _all_edges.row(idx);
+
+        for (size_t jdx = idx + 1; jdx < num_all_edges; ++jdx) {
+            if (labels(idx) == labels(jdx)) {
+                continue;
+            }
+
+            const arma::rowvec edge2 = _all_edges.row(jdx);
+            double lk = filamentFields::compute_linking_number_for_edges(edge1, edge2);
+            entanglement_matrix(idx, jdx) = lk;
+        }
+    }
+}
